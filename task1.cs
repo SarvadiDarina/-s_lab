4290c277d0d11785122f6d6bfe2fdf433ed59c54
SarvadiDarina <56667667+SarvadiDarina@users.noreply.github.com>
2020-02-23 19:14:06 +0200

    lab1_4

---
+++ task1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    	{
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Angle = ");
+            double angle = Convert.ToInt32(Console.ReadLine());
+            double result = 0.5 * a * b * Math.Sin((angle * Math.PI) / 180);
+            Console.WriteLine("Площа трикутника = {0}", result);
+
+        }
+    }
+}
